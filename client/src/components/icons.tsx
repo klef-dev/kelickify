cc0f4b4e8e15fb145991c118603b58ba96cda165
Abraham <abrahamugbeshe@gmail.com>
2025-01-17 16:20:25 +0100

    feat: add all needed icons

---
+++ client/src/components/icons.tsx
@@ -0,0 +1,278 @@
+import { SVGProps } from "react";
+
+export const KelickHomeIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M9 20.9926H7C4.79086 20.9926 3 19.2017 3 16.9926V10.7002C3 9.30145 3.73061 8.00433 4.92679 7.27938L9.92679 4.24907C11.2011 3.4768 12.7989 3.47679 14.0732 4.24907L19.0732 7.27938C20.2694 8.00433 21 9.30145 21 10.7002V16.9926C21 19.2017 19.2091 20.9926 17 20.9926H15M9 20.9926V16.9926C9 15.3357 10.3431 13.9926 12 13.9926V13.9926C13.6569 13.9926 15 15.3357 15 16.9926V20.9926M9 20.9926H15"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const KelickOrgIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M7 9.00258L7.01 8.99147" stroke="#2E3333" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      d="M11 9.00258L11.01 8.99147"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path d="M7 13.0026L7.01 12.9915" stroke="#2E3333" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      d="M11 13.0026L11.01 12.9915"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path d="M7 17.0026L7.01 16.9915" stroke="#2E3333" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      d="M11 17.0026L11.01 16.9915"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M15 20.9926H3.6C3.26863 20.9926 3 20.724 3 20.3926V5.59258C3 5.26121 3.26863 4.99258 3.6 4.99258H9V3.59258C9 3.26121 9.26863 2.99258 9.6 2.99258H14.4C14.7314 2.99258 15 3.26121 15 3.59258V8.99258M15 20.9926H20.4C20.7314 20.9926 21 20.724 21 20.3926V9.59258C21 9.26121 20.7314 8.99258 20.4 8.99258H15M15 20.9926V16.9926M15 8.99258V12.9926M15 12.9926H17M15 12.9926V16.9926M15 16.9926H17"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const KelickUsersIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M7 17.9926V16.9926C7 14.2312 9.23858 11.9926 12 11.9926V11.9926C14.7614 11.9926 17 14.2312 17 16.9926V17.9926"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M1 17.9926V16.9926C1 15.3357 2.34315 13.9926 4 13.9926V13.9926"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M23 17.9926V16.9926C23 15.3357 21.6569 13.9926 20 13.9926V13.9926"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M12 11.9926C13.6569 11.9926 15 10.6494 15 8.99258C15 7.33573 13.6569 5.99258 12 5.99258C10.3431 5.99258 9 7.33573 9 8.99258C9 10.6494 10.3431 11.9926 12 11.9926Z"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M4 13.9926C5.10457 13.9926 6 13.0972 6 11.9926C6 10.888 5.10457 9.99258 4 9.99258C2.89543 9.99258 2 10.888 2 11.9926C2 13.0972 2.89543 13.9926 4 13.9926Z"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M20 13.9926C21.1046 13.9926 22 13.0972 22 11.9926C22 10.888 21.1046 9.99258 20 9.99258C18.8954 9.99258 18 10.888 18 11.9926C18 13.0972 18.8954 13.9926 20 13.9926Z"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const KelickSendCashIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M10 8.22335C9.20085 7.48567 7.79346 6.97257 6.5 6.94044M3 15.2234C3.75189 16.1488 5.1499 16.6766 6.5 16.7214M6.5 6.94044C4.96102 6.90221 3.58333 7.54487 3.58333 9.30028C3.58333 12.531 10 10.9157 10 14.1464C10 15.9891 8.2922 16.7808 6.5 16.7214M6.5 6.94044V4.99258M6.5 16.7214V18.9926"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M13 11.9926H21M21 11.9926L17.16 7.99258M21 11.9926L17.16 15.9926"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const KelickCalendarIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M15 3.99258V1.99258M15 3.99258V5.99258M15 3.99258H10.5M3 9.99258V18.9926C3 20.0972 3.89543 20.9926 5 20.9926H19C20.1046 20.9926 21 20.0972 21 18.9926V9.99258H3Z"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M3 9.99258V5.99258C3 4.88801 3.89543 3.99258 5 3.99258H7"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path d="M7 1.99258V5.99258" stroke="#2E3333" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      d="M21 9.99258V5.99258C21 4.88801 20.1046 3.99258 19 3.99258H18.5"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const KelickReceiveCashIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M10 8.22335C9.20085 7.48567 7.79346 6.97257 6.5 6.94044M3 15.2234C3.75189 16.1488 5.1499 16.6766 6.5 16.7214M6.5 6.94044C4.96102 6.90221 3.58333 7.54487 3.58333 9.30028C3.58333 12.531 10 10.9157 10 14.1464C10 15.9891 8.2922 16.7808 6.5 16.7214M6.5 6.94044V4.99258M6.5 16.7214V18.9926"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M21 11.9926H13M13 11.9926L16.84 7.99258M13 11.9926L16.84 15.9926"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const KelickMoreIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M18 12.4926C18.2761 12.4926 18.5 12.2687 18.5 11.9926C18.5 11.7164 18.2761 11.4926 18 11.4926C17.7239 11.4926 17.5 11.7164 17.5 11.9926C17.5 12.2687 17.7239 12.4926 18 12.4926Z"
+      fill="#2E3333"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M12 12.4926C12.2761 12.4926 12.5 12.2687 12.5 11.9926C12.5 11.7164 12.2761 11.4926 12 11.4926C11.7239 11.4926 11.5 11.7164 11.5 11.9926C11.5 12.2687 11.7239 12.4926 12 12.4926Z"
+      fill="#2E3333"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M6 12.4926C6.27614 12.4926 6.5 12.2687 6.5 11.9926C6.5 11.7164 6.27614 11.4926 6 11.4926C5.72386 11.4926 5.5 11.7164 5.5 11.9926C5.5 12.2687 5.72386 12.4926 6 12.4926Z"
+      fill="#2E3333"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const KelickWalletIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M19 20H5C3.89543 20 3 19.1046 3 18V9C3 7.89543 3.89543 7 5 7H19C20.1046 7 21 7.89543 21 9V18C21 19.1046 20.1046 20 19 20Z"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+    />
+    <path
+      d="M16.5 14C16.2239 14 16 13.7761 16 13.5C16 13.2239 16.2239 13 16.5 13C16.7761 13 17 13.2239 17 13.5C17 13.7761 16.7761 14 16.5 14Z"
+      fill="#2E3333"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M18 7V5.60322C18 4.28916 16.7544 3.33217 15.4847 3.67075L4.48467 6.60409C3.60917 6.83756 3 7.63046 3 8.53656V9"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+    />
+  </svg>
+);
+
+export const KelickBulkUploadIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M22 14V8.5M6 13V6C6 4.34315 7.34315 3 9 3H14"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M16.9922 4H19.9922M22.9922 4L19.9922 4M19.9922 4V1M19.9922 4V7"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M12 21H6C3.79086 21 2 19.2091 2 17C2 14.7909 3.79086 13 6 13H17H18C15.7909 13 14 14.7909 14 17C14 19.2091 15.7909 21 18 21C20.2091 21 22 19.2091 22 17V14"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const KelickAddUserIcon = (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M17 10H20M23 10H20M20 10V7M20 10V13"
+      stroke="#F9FCFC"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M1 20V19C1 15.134 4.13401 12 8 12V12C11.866 12 15 15.134 15 19V20"
+      stroke="#F9FCFC"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12Z"
+      stroke="#F9FCFC"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+
+export const KelickCircleCheckIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M7 12.5L10 15.5L17 8.5" stroke="#2E3333" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+      stroke="#2E3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
